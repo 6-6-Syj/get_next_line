ec3022ca3895d643b7c1e4d41bad5c2119f56d70
6-6-Syj <magand.jessy@outlook.fr>
2024-11-27 16:43:47 +0100

    hgfhcg

--- get_next_line.c
+++ get_next_line.c
@@ -12,49 +12,56 @@
 
 #include "get_next_line.h"
 
-char	*set_line(char *line_buf)
+static char	*set_line(char *line_buf)
 {
 	size_t	i;
+	size_t	len;
 	char	*left_from_buf;
 
+	len = ft_strlen(line_buf);
 	i = 0;
 	while (line_buf[i])
 	{
-		if (line_buf[i])
-		if (line_buf[i] == '\n' && !(line_buf[i + 1]))
-			// printf("Test K1 => %s", ft_substr(line_buf, 0, i));
-			return (ft_substr(line_buf, 0, i));
-		if (line_buf[i] == '\n' && line_buf[i + 1]) // problem about (i + 1),
-			// can replace a char to add \0... need to see.
-			line_buf[i + 1] = '\0';
+		if (i <= BUFFER_SIZE && line_buf[i] == '\n')
+		{
+			if (i == 0)
+				left_from_buf = ft_substr(line_buf, i, len + 1);
+			left_from_buf = ft_substr(line_buf, i + 1, len + 1);
+			printf("left_from_buf => %s", left_from_buf);
+			return (left_from_buf);
+		}
 		i++;
 	}
-	printf("Test K2 => %s", ft_substr(line_buf, 0, i));
-	i = 0;
-	while (line_buf[i])
-	{
-		// printf("%zu\n", i);
-		i++;
-	}
-	return (ft_strdup(""));
+	left_from_buf = ft_strdup(line_buf);
+	printf("left_from_buf => %s", left_from_buf);
+	return (left_from_buf);
 }
 
+static char	*fill_line_buffer(int fd, char *left_from_buf, char *buf)
+{
+	return (0);
+}
 // READ, FREE, MALLOC
 char	*get_next_line(int fd)
 {
-	// Recuperer le contenu du buffer et le copy/paste --> *char
+	// DONE --- Recuperer le contenu du buffer et le copy/paste --> *left_from_buf
 	// search '\n', si R, rappeler read ---- si y'a : extraire la ligne ET le '\n'
 	// Ensuite clear le *char
 	// Variable static --> garde ce qu'il y avait avant dans *char
-	static char buf[BUFFER_SIZE + 10];
+	static char buf[BUFFER_SIZE + 1];
 	char *line_buf;
 
 	// printf("READ 1 = %zd\n", read(fd, buf, BUFFER_SIZE));
-	buf[read(fd, buf, BUFFER_SIZE)] = '\0';
+	read(fd, buf, BUFFER_SIZE);
+
+	line_buf = malloc(BUFFER_SIZE + 1 * sizeof(char));
+	if (!line_buf)
+		return (free(line_buf), NULL);
 	line_buf = buf;
-	printf("buf = %s\n", buf);
+
 	// printf("line_buf = %s\n", buf);
 	// printf("return read = %zi\n", read(fd, buf, BUFFER_SIZE));
+	printf("buf = %s\n", buf);
 	set_line(line_buf);
 	return (0);
 }
@@ -67,6 +74,16 @@ int main()
 	fd = open("test.txt", O_RDONLY);
 	// if (fd == -1)
 	// 	return (1);
+	get_next_line(fd);
+	printf("\n -------------- \n");
+	get_next_line(fd);
+	printf("\n -------------- \n");
+	get_next_line(fd);
+	printf("\n -------------- \n");
+	get_next_line(fd);
+	printf("\n -------------- \n");
+	get_next_line(fd);
+	printf("\n -------------- \n");
 	get_next_line(fd);
 	printf("\n -------------- \n");
 	get_next_line(fd);
